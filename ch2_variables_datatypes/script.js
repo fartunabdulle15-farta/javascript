af9a656c410c63644d5dee240b482ba86ba3afbb
fartun omar <fartunabdulle15@gmail.com>
2026-06-13 03:24:24 -0700

    i added ch2_variables_datatypes

---
+++ ch2_variables_datatypes/script.js
@@ -0,0 +1,13 @@
+// var firstName = "Fartun" // string
+// var lastName = "Omar" // string
+// var age = 20 // number
+// var isStudent = true // boolean
+// console.log(firstName * lastName )
+// console.log(lastName)
+// console.log(age * age)
+// console.log(isStudent)
+// 
+var salary = 5000
+var tax = 500
+var takeHome =salary - 500
+console.log(takeHome)
