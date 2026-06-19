a50b36808390025f5149d26dbf3d63eff1a52341
fartun omar <fartunabdulle15@gmail.com>
2026-06-19 10:40:29 -0700

    ive added ch5 where i learned scopes, var, let, const

---
+++ ch5_scopes_var_let_const/variables.js
@@ -0,0 +1,21 @@
+//   var first_name =  "fartun"
+//   let age =20
+
+//  let isSingle = true
+
+//  console.log(first_name)
+//  console.log(age)
+//  console.log(isSingle)
+
+
+
+// global scope
+let firstName = "fartun"
+
+
+
+// local scope
+{
+    let lastName = "omar"
+    console.log(firstName, lastName, )
+}
